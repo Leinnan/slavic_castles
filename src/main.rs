d537e602d86a1a6fb71d7bbb47bd850ffbd12de6
Piotr Siuszko <siuszko@zoho.com>
2019-12-12 21:49:18 +0100

    Change resize strategy

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,14 @@ use quicksilver::{
 };
 
 fn main() {
+    std::env::set_var("WINIT_HIDPI_FACTOR", "1.0");
     run::<MyGame>(
         "Slavic castles!",
         Vector::new(consts::SCREEN_WIDTH, consts::SCREEN_HEIGHT),Settings {
-            max_updates: 60,
+            scale: quicksilver::graphics::ImageScaleStrategy::Blur,
+            resize: quicksilver::graphics::ResizeStrategy::Maintain,
+            draw_rate:  16.0,  // 16ms per draw = 60 draw per second
+            max_updates: 60, // Maximum updates per frame
             ..Settings::default()
         });
 }
